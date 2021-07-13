78148122df600096a1694320fb9f2b2771ef58b8
Vance Lucas <vance@vancelucas.com>
2021-07-13 16:37:43 -0500

    Export SeamlessQuery type from index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { SeamlessClient } from './shared/SeamlessClient';
-export type { SeamlessClientConfig } from './shared/SeamlessClient';
+export type { SeamlessClientConfig, SeamlessQuery } from './shared/SeamlessClient';
 export { sql } from './shared/sql';
